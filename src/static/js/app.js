53a372e1049877c9974d176c59bc8f5384952e19
Francois Gaudin <contact@francoisgaudin.com>
2013-11-16 12:20:33 -0800

    Like

--- src/static/js/app.js
+++ src/static/js/app.js
@@ -1,13 +1,13 @@
 window.App = Ember.Application.create({
-	LOG_TRANSITIONS: true
+    LOG_TRANSITIONS: true
 });
 
 App.ApplicationAdapter = DS.FixtureAdapter.extend();
 
 App.Router.map(function () {
   this.resource('libre', { path: '/' }, function(){
-	  this.route('login',  { path: '/login' });
-	  this.resource("message", { path: "/message/:message_id" });
+      this.route('login',  { path: '/login' });
+      this.resource("message", { path: "/message/:message_id" });
   });
 });
 
@@ -15,7 +15,8 @@ App.Message = DS.Model.extend({
   content1: DS.attr('string'),
   author: DS.attr('string'),
   date: DS.attr('date'),
-  
+  liked: DS.attr('boolean'),
+
   formatedDate: function() {
     return this.get('date').toLocaleString();
   }.property('date')
@@ -26,19 +27,22 @@ App.Message.FIXTURES = [
      id: 1,
      content1: 'Tennessee Pastor Disputes Wildlife Possession Charge by State http://nyti.ms/1eXMq9H ',
      author: 'The New York Times',
-     date: new Date('2013-01-01')
+     date: new Date('2013-01-01'),
+     liked: false
    },
    {
      id: 2,
      content1: "Il s'en est passé des choses cette semaine sur @franceinter ! Quoi ?... Réponse sur http://bit.ly/rambo72  #rambobino pic.twitter.com/kxHx4oaOIm",
      author: "France Inter",
-     date: new Date('2013-01-01')
+     date: new Date('2013-01-01'),
+        liked: true
    },
    {
      id: 3,
      content1: "Bon, fin de l'analyse politique de comptoir, je vais aller chercher le pain.!",
      author: "Padre Pio",
-     date: new Date('2013-01-01')
+     date: new Date('2013-01-01'),
+     liked: false
    }
   ];
 
@@ -55,21 +59,28 @@ App.LibreIndexRoute = Ember.Route.extend({
 });
 
 App.LibreIndexController = Ember.ArrayController.extend({
-	sortProperties: ['id'],
-	sortAscending: false,
-	actions: {
-	    createMessage: function () {
-	      var content1 = this.get('newMessage');
-	      if (!content1.trim()) { return; }
+    sortProperties: ['id'],
+    sortAscending: false,
+    actions: {
+        createMessage: function () {
+          var content1 = this.get('newMessage');
+          if (!content1.trim()) { return; }
 
-	      var message = this.store.createRecord('message', {
-	    	id: 4,
-	        content1: content1,
-	        author: 'me',
-	        date: new Date()
-	      });
-	      this.set('newMessage', '');
-	      message.save();
-	    }
-	}
+          var message = this.get('store').createRecord('message', {
+            id: 4,
+            content1: content1,
+            author: 'me',
+            date: new Date()
+          });
+          this.set('newMessage', '');
+          message.save();
+        },
+        toggleLike: function(message_id){
+        this.store.find('message', message_id).then(function(message){
+                var value = !message.get('liked');
+                message.set('liked', value);
+                message.save();
+            });
+        }
+    }
 });
