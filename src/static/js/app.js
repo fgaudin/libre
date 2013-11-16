b945a1253de4b33d34611918f2c0402960201ab5
Francois Gaudin <contact@francoisgaudin.com>
2013-11-16 00:25:05 -0800

    Added the date

--- src/static/js/app.js
+++ src/static/js/app.js
@@ -7,24 +7,28 @@ App.Router.map(function () {
 
 App.Message = DS.Model.extend({
   content: DS.attr('string'),
-  author: DS.attr('string')
+  author: DS.attr('string'),
+  date: DS.attr('date')
 });
 
 App.Message.FIXTURES = [
    {
      id: 1,
      content: 'Tennessee Pastor Disputes Wildlife Possession Charge by State http://nyti.ms/1eXMq9H ',
-     author: 'The New York Times'
+     author: 'The New York Times',
+     date: new Date('2013-01-01')
    },
    {
      id: 2,
      content: "Il s'en est passé des choses cette semaine sur @franceinter ! Quoi ?... Réponse sur http://bit.ly/rambo72  #rambobino pic.twitter.com/kxHx4oaOIm",
-     author: "France Inter"
+     author: "France Inter",
+     date: new Date('2013-01-01')
    },
    {
      id: 3,
      content: "Bon, fin de l'analyse politique de comptoir, je vais aller chercher le pain.!",
-     author: "Padre Pio"
+     author: "Padre Pio",
+     date: new Date('2013-01-01')
    }
   ];
 
@@ -45,7 +49,8 @@ App.MessagesController = Ember.ArrayController.extend({
 	      var message = this.store.createRecord('message', {
 	    	id: 4,
 	        content: content,
-	        author: 'me'
+	        author: 'me',
+	        date: new Date()
 	      });
 
 	      // Clear the "New Todo" text field
