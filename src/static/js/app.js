f9d38c263c1255610408a7cd8e1ee998a721337f
Francois Gaudin <contact@francoisgaudin.com>
2013-11-16 14:52:50 -0800

    Reuse message controller

--- src/static/js/app.js
+++ src/static/js/app.js
@@ -7,12 +7,14 @@ App.ApplicationAdapter = DS.FixtureAdapter.extend();
 App.Router.map(function () {
   this.resource('libre', { path: '/' }, function(){
       this.route('login',  { path: '/login' });
-      this.resource("message", { path: "/message/:message_id" });
+      this.resource("message", { path: "/message/:message_id" }, function(){
+      this.route('create',  { path: '/message/new' });
+      });
   });
 });
 
 App.Message = DS.Model.extend({
-  content1: DS.attr('string'),
+  body: DS.attr('string'),
   author: DS.attr('string'),
   date: DS.attr('date'),
   liked: DS.attr('boolean'),
@@ -25,21 +27,21 @@ App.Message = DS.Model.extend({
 App.Message.FIXTURES = [
    {
      id: 1,
-     content1: 'Tennessee Pastor Disputes Wildlife Possession Charge by State http://nyti.ms/1eXMq9H ',
+     body: 'Tennessee Pastor Disputes Wildlife Possession Charge by State http://nyti.ms/1eXMq9H ',
      author: 'The New York Times',
      date: new Date('2013-01-01'),
      liked: false
    },
    {
      id: 2,
-     content1: "Il s'en est passé des choses cette semaine sur @franceinter ! Quoi ?... Réponse sur http://bit.ly/rambo72  #rambobino pic.twitter.com/kxHx4oaOIm",
+     body: "Il s'en est passé des choses cette semaine sur @franceinter ! Quoi ?... Réponse sur http://bit.ly/rambo72  #rambobino pic.twitter.com/kxHx4oaOIm",
      author: "France Inter",
      date: new Date('2013-01-01'),
         liked: true
    },
    {
      id: 3,
-     content1: "Bon, fin de l'analyse politique de comptoir, je vais aller chercher le pain.!",
+     body: "Bon, fin de l'analyse politique de comptoir, je vais aller chercher le pain.!",
      author: "Padre Pio",
      date: new Date('2013-01-01'),
      liked: false
@@ -62,25 +64,28 @@ App.LibreIndexController = Ember.ArrayController.extend({
     sortProperties: ['id'],
     sortAscending: false,
     actions: {
-        createMessage: function () {
-          var content1 = this.get('newMessage');
-          if (!content1.trim()) { return; }
-
-          var message = this.get('store').createRecord('message', {
-            id: 4,
-            content1: content1,
-            author: 'me',
-            date: new Date()
-          });
-          this.set('newMessage', '');
-          message.save();
-        },
         toggleLike: function(message_id){
         this.store.find('message', message_id).then(function(message){
                 var value = !message.get('liked');
                 message.set('liked', value);
-                message.save();
             });
+        }
+    }
+});
+
+App.MessageCreateController = Ember.ObjectController.extend({
+    actions: {
+        create: function () {
+          var body = this.get('newMessage');
+          if (!body.trim()) { return; }
+
+          var message = this.get('store').createRecord('message', {
+            id: 4,
+            body: body,
+            author: 'me',
+            date: new Date()
+          });
+          this.set('newMessage', '');
         }
     }
 });
